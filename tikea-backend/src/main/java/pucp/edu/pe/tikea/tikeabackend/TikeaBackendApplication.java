78a5f1d487685fc0df5672cca78f135e8d9cd3cd
Alfredo123255 <a20220825@pucp.edu.pe>
2025-11-05 16:01:20 -0600

    CREACION PROYECTO

---
+++ tikea-backend/src/main/java/pucp/edu/pe/tikea/tikeabackend/TikeaBackendApplication.java
@@ -0,0 +1,13 @@
+package pucp.edu.pe.tikea.tikeabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikeaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikeaBackendApplication.class, args);
+    }
+
+}
---
+++ tikea-backend/src/test/java/pucp/edu/pe/tikea/tikeabackend/TikeaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pucp.edu.pe.tikea.tikeabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikeaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
